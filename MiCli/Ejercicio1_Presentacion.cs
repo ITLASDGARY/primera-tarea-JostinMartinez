894b8c5db92ca77887f1780ad4e4efc69d6c41f9
JostinMartinez <139613152+JostinMartinez@users.noreply.github.com>
2026-01-23 15:29:46 +0000

    Ejercios Arreglados

--- MiCli/Ejercicio1_Presentacion.cs
+++ MiCli/Ejercicio1_Presentacion.cs
@@ -5,27 +5,20 @@ public class Ejercicio1_Presentacion
 {
     public static string ObtenerPresentacion()
     {
-        // TODO: Declara tres variables string: nombre, carrera y hobby
+     
         string nombre = "Jostin";
         string carrera = "Desarrollo de software";
-        string hobby = "Escuchar musica y leer revistas tecnologicas";
-        
-<<<<<<< HEAD
-string nombre = "Jostin ";
-string carrera = "Desarollo de software ";
-string hoobie = "Escuchar musica y leer revistas tecnologicas";
-string resultado = "Mi nombre es " + nombre  +  "\n Mi carrera es " + carrera + " \n Mi Hobbie es " + hoobie;
-        // TODO: Retorna un string con las tres variables en líneas separadas
-        return resultado; //OJO: reemplaza este texto
-        throw new NotImplementedException();
-=======
-        // TODO: Retorna un string con las tres variables en líneas separadas
-        return nombre + "\n" + carrera + "\n" + hobby;
->>>>>>> 5f326f79e5395ff6a01355cbc2e2dc867c534dd5
+        string hobby = "Escuchar música y leer revistas tecnológicas";
+
+      
+        return "Mi nombre es " + nombre +
+               "\nMi carrera es " + carrera +
+               "\nMi hobby es " + hobby;
     }
-    
+
     public static void Ejecutar()
     {
         Console.WriteLine(ObtenerPresentacion());
-    }
+    }    
+
 }
