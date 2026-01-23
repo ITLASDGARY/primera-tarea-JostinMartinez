b7c5aee583aa1fe00595661834c70d6af9789e7f
JostinMartinez <139613152+JostinMartinez@users.noreply.github.com>
2026-01-23 00:27:10 +0000

    Checkpoint from VS Code for coding agent session

--- MiCli/Ejercicio1_Presentacion.cs
+++ MiCli/Ejercicio1_Presentacion.cs
@@ -6,6 +6,11 @@ public class Ejercicio1_Presentacion
     public static string ObtenerPresentacion()
     {
         // TODO: Declara tres variables string: nombre, carrera y hobby
+        
+string nombre = "Jostin ";
+string carrera = "Desarollo de software ";
+string hoobie = "Escuchar musica y leer revistas tecnologicas";
+Console.WriteLine("Mi nombre es " + nombre  +  "\n Mi edad es" + edad + " \n Mi Hobbie es" + hoobie);
         // TODO: Retorna un string con las tres variables en líneas separadas
         return "reemplaza este texto con tu presentación"; //OJO: reemplaza este texto
         throw new NotImplementedException();
